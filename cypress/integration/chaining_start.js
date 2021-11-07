f9064e6249425288dd226eadaddf5b2fcb6bc2b8
naima94 <naimatou.adambi@gmail.com>
2021-11-07 10:21:29 -0800

    Initial

---
+++ cypress/integration/chaining_start.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+
+  cy
+    .visit('/board/43980845040')
+})
+
+it('Chaining commands', () => {
+
+  cy
+    .get('[data-cy="task"]')
+    .should('be.visible')
+
+});
